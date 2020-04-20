ffec18b33b7543d675deb3ef748d8468249fd27f
junilHwang <tjsdlf4261@naver.com>
2020-04-20 22:45:05 +0900

    GraphQL 설정 중

--- nestjs-graphql/src/app.module.ts
+++ nestjs-graphql/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gql',
+      debug: true,
+      playground: true
+    })
+  ]
 })
 export class AppModule {}
