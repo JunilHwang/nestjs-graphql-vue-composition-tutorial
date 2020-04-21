2c985cbff8f650334d7bd0770ba6302c808d8289
junilHwang <tjsdlf4261@naver.com>
2020-04-21 23:15:32 +0900

    User Module 작성 중

---
+++ nestjs-graphql/src/user/user.model.ts
@@ -0,0 +1,16 @@
+import {Field, ID, ObjectType} from "@nestjs/graphql";
+
+@ObjectType()
+export class User {
+  @Field(() => ID)
+  idx: number
+
+  @Field()
+  id: string
+
+  @Field()
+  email: string
+
+  @Field()
+  name: string
+}
