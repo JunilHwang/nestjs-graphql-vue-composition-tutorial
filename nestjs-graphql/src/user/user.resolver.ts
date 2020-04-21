e23b1edd9b8e296a60d5a06e718f3f6c9acfd8d1
junilHwang <tjsdlf4261@naver.com>
2020-04-21 23:25:12 +0900

    User Resolver 작성 중

--- nestjs-graphql/src/user/user.resolver.ts
+++ nestjs-graphql/src/user/user.resolver.ts
@@ -1,9 +1,19 @@
-import {Resolver} from "@nestjs/graphql";
-import {User} from "./user.model";
+import {Mutation, Query, Resolver} from '@nestjs/graphql'
+import {User} from './user.model'
+import { UserService } from './user.service'
 
 @Resolver(() => User)
 export class UserResolver {
 
-  constructor (private readonly userService: UserService) {
+  constructor (private readonly userService: UserService) {}
+
+  @Query(() => User)
+  async find (idx: number): Promise<User> {
+    return await this.userService.find(idx)
+  }
+
+  @Query(() => [User])
+  async findAll (): Promise<User[]> {
+    return await this.userService.findAll()
   }
 }
