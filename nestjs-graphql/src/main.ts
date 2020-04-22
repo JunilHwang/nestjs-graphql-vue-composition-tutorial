28964ade21057405e81fed06d124cfc2eee24d04
junilHwang <tjsdlf4261@naver.com>
2020-04-22 22:25:17 +0900

    의존성 추가

--- nestjs-graphql/src/main.ts
+++ nestjs-graphql/src/main.ts
@@ -1,9 +1,10 @@
-import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common'
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { NestExpressApplication } from '@nestjs/platform-express'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3000);
