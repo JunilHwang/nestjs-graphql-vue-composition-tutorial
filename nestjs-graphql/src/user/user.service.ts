6d72c264db0aff3b163310df3b95a1df2fb73414
junilHwang <tjsdlf4261@naver.com>
2020-04-22 22:49:38 +0900

    간단한 User Service Logic 작성

--- nestjs-graphql/src/user/user.service.ts
+++ nestjs-graphql/src/user/user.service.ts
@@ -4,16 +4,34 @@ import { UserInput } from './user.input'
 
 @Injectable()
 export class UserService {
+  private users: User[] = []
+  private lastIdx: number = 1
+
   async find (idx: number): Promise<User> {
-    return Promise.resolve({} as any)
+    return Promise.resolve(this.users.find(v => v.idx === idx))
   }
   async findAll (): Promise<User[]> {
-    return Promise.resolve([])
+    return Promise.resolve(this.users)
   }
-  async create (userInput: UserInput): Promise<User> {
-    return Promise.resolve({} as any)
+  async create ({ id, email, name }: UserInput): Promise<User> {
+    try {
+      const user = new User()
+      user.idx = this.lastIdx
+      user.id = id
+      user.email = email
+      user.name = name
+      this.lastIdx += 1
+      this.users.push(user)
+      return Promise.resolve(user)
+    } catch (e) {
+      this.lastIdx -= 1
+    }
   }
   async remove (idx: number): Promise<Boolean> {
+    const k = this.users.findIndex(v => v.idx === idx)
+    if (k === -1) return Promise.resolve(false)
+
+    this.users.splice(k, 1)
     return Promise.resolve(true)
   }
 }
