9d448b61beba5b92e835f5451ff235f4cb29f5cc
junilHwang <tjsdlf4261@naver.com>
2020-04-22 23:54:35 +0900

    docker compose 수정

--- nestjs-graphql/src/database/mongodb/index.ts
+++ nestjs-graphql/src/database/mongodb/index.ts
@@ -1,6 +1,6 @@
 import { MongooseModule } from '@nestjs/mongoose';
 const HOST = 'localhost',
-      PORT = '27017',
+      PORT = '27018',
       DBNAME = 'nestjs_graphql_tutorial'
 
 const uri = `mongodb://${HOST}:${PORT}/${DBNAME}`;
