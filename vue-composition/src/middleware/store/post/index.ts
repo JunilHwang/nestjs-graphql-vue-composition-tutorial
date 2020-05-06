bf85ebacbad6e92ae8932193a32a324a774c001d
junilHwang <tjsdlf4261@naver.com>
2020-05-06 22:54:11 +0900

    feat: Post Stub Data로 테스트 중

--- vue-composition/src/middleware/store/post/index.ts
+++ vue-composition/src/middleware/store/post/index.ts
@@ -1,15 +1,30 @@
-import {Action, Module, MutationAction, VuexModule} from 'vuex-module-decorators'
-import { Post, PostVO } from 'domain/types'
+import { Action, Module, MutationAction, VuexModule } from 'vuex-module-decorators'
+import { Post, PostVO, User } from 'domain/types'
+
+const postList: Post[] = []
+const user: User = {
+  id: `${Math.random() * 10000}`,
+  userId: 'junil',
+  name: '황준일',
+  email: 'junil.h@kakao.com',
+  createdAt: new Date(),
+}
 
 @Module({
   namespaced: true
 })
 export default class UserStore extends VuexModule {
 
-  postList!: Post[]
+  postList: Post[] = postList
 
-  @Action
-  ADD_POST (postDetail: PostVO) {
-    console.log(postDetail)
+  @MutationAction
+  async ADD_POST (postDetail: PostVO) {
+    postList.push({
+      ...postDetail,
+      id: `${Math.random() * 10000}`,
+      writer: user,
+      createdAt: new Date()
+    })
+    return { postList }
   }
 }
