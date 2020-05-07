7572190aa5f7b45ce576900080fce8ee15b6c9d0
junilHwang <tjsdlf4261@naver.com>
2020-05-07 23:06:57 +0900

    refactor: PostList를 직접 할당

--- vue-composition/src/middleware/store/post/index.ts
+++ vue-composition/src/middleware/store/post/index.ts
@@ -1,45 +1,40 @@
-import {Action, Module, Mutation, MutationAction, VuexModule} from 'vuex-module-decorators'
+import { Action, Module, Mutation, VuexModule } from 'vuex-module-decorators'
 import { Post, PostVO } from 'domain/types'
 import { getRandId } from '@/helper'
 import { user as writer } from '../user'
 
-const postList: Post[] = [
-  {
-    id: getRandId(),
-    title: 'test',
-    content: 'test\ncontent',
-    createdAt: new Date(),
-    writer
-  }
-]
-
-@Module({
-  namespaced: true
-})
+@Module({ namespaced: true })
 export default class UserStore extends VuexModule {
 
-  postList: Post[] = postList
+  postList: Post[] = [{
+      id: getRandId(),
+      title: 'test',
+      content: 'test\ncontent',
+      createdAt: new Date(),
+      writer
+    }
+  ]
 
   get posts (): Post[] {
     return this.postList
   }
 
   @Mutation
-  SET_POST_LIST (postList: Post[]) {
+  SET_LIST (postList: Post[]) {
     this.postList = postList
   }
 
-  @Action({ commit: 'SET_POST_LIST' })
+  @Action({ commit: 'SET_LIST' })
   async ADD_POST (postDetail: PostVO) {
     return [ ...this.posts, {
       ...postDetail,
       id: getRandId(),
       createdAt: new Date(),
       writer
-    } ]
+    }]
   }
 
-  @Action({ commit: 'SET_POST_LIST' })
+  @Action({ commit: 'SET_LIST' })
   async DELETE_POST ({ id }: Post) {
     return this.posts.filter(v => v.id !== id)
   }
